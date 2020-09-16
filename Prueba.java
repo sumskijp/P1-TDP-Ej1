43b720e71f2f0df794bc8a9e42bbd483568cf895
sumskijp <juanpablosumski@hotmail.com>
2020-09-16 16:34:44 -0300

    tercer commit, modificacion local

--- Prueba.java
+++ Prueba.java
@@ -1,7 +1,7 @@
 public class Prueba{
 	
 	public static void main(String[] args){
-		Numero test = new Numero(12);
+		Numero test = new Numero(15);
 		System.out.println("Test: "+test.getNum());
 	}
 	
