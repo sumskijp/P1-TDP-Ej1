581c597ed937d54dcd2606e6b90cefed333c8100
sumskijp <juanpablosumski@hotmail.com>
2020-09-16 16:29:08 -0300

    segundo commit, modificacion remota

--- Prueba.java
+++ Prueba.java
@@ -1,7 +1,7 @@
 public class Prueba{
 	
 	public static void main(String[] args){
-		Numero test = new Numero(12);
+		Numero test = new Numero(21);
 		System.out.println("Test: "+test.getNum());
 	}
 	
