57454fbea5389baf4704b1feb77cd05127c8d6a0
sumskijp <juanpablosumski@hotmail.com>
2020-09-16 16:07:02 -0300

    commit punto e

---
+++ Numero.java
@@ -0,0 +1,18 @@
+public class Numero{
+	private int n;
+	
+	public Numero(int a){
+		n = a;
+	}
+	public Numero(){
+		n = 0;
+	}
+	
+	public void setNum(int a){
+		n = a;
+	}
+	
+	public int getNum(){
+		return n;
+	}
+}
---
+++ Prueba.java
@@ -0,0 +1,8 @@
+public class Prueba{
+	
+	public static void main(String[] args){
+		Numero test = new Numero(12);
+		System.out.println("Test: "+test.getNum());
+	}
+	
+}
